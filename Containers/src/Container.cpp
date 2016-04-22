dcd9e5313f87760293da6e513c65a713c48e537d
raisantos <raisoledade@outlook.com>
2016-04-22 18:31:37 -0400

    PP1

---
+++ Containers/src/Container.cpp
@@ -0,0 +1,18 @@
+/*
+ * Container.cpp
+ *
+ *  Created on: 22 de abr de 2016
+ *      Author: raisantos
+ */
+
+#include "Container.h"
+
+Container::Container() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Container::~Container() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Containers/src/Container.h
@@ -0,0 +1,17 @@
+/*
+ * Container.h
+ *
+ *  Created on: 22 de abr de 2016
+ *      Author: raisantos
+ */
+
+#ifndef CONTAINER_H_
+#define CONTAINER_H_
+
+class Container {
+public:
+	Container();
+	virtual ~Container();
+};
+
+#endif /* CONTAINER_H_ */
